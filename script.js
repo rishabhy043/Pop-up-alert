50c72a7f80b390b761757c1496a9607a2035c2bd
rishabhy043 <rishabh.knipss043@gmail.com>
2024-11-30 22:43:50 +0530

    script

---
+++ script.js
@@ -0,0 +1,8 @@
+const button = document.querySelector('button')
+const closeIcon = document.querySelector('.close-icon')
+const popup = document.querySelector('.popup')
+const popupContainer = document.querySelector('.popup-container')
+button.addEventListener('click', () => {
+    popupContainer.classList.add('popup-open')
+    popupContainer.classList.add('popup-open')
+})
